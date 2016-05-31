12b0d2df77dc8b373838e162d132d343a82fa8d1
Shayan Shahand <shayanlinux@users.noreply.github.com>
2016-05-31 22:33:01 +0200

    breake Authenticate class into Authenticate and Security Controllers

---
+++ app/nl/amc/ebioscience/rosemary/controllers/Authenticate.scala
@@ -0,0 +1,69 @@
+package nl.amc.ebioscience.rosemary.controllers
+
+import javax.inject._
+import scala.concurrent.duration._
+import nl.amc.ebioscience.rosemary.models.User
+import play.api.data.Form
+import play.api.data.Forms.{ email, mapping, nonEmptyText }
+import play.api.libs.json.Json
+import play.api.i18n.{ MessagesApi, Messages, Lang }
+import play.api.cache.CacheApi
+import play.api.Configuration
+import play.api.mvc._
+
+@Singleton
+class Authenticate @Inject() (configuration: Configuration, messagesApi: MessagesApi, cacheApi: CacheApi)
+    extends Controller {
+
+  private implicit def messages(implicit lang: Lang) = new Messages(lang, messagesApi)
+
+  private val AuthTokenHeader = "X-XSRF-TOKEN"
+  private val AuthTokenCookieKey = "XSRF-TOKEN"
+
+  private lazy val CacheExpiration =
+    configuration.getInt("cache.expiration").getOrElse(60 /*seconds*/ * 2 /* minutes */ ).seconds
+
+  case class Login(email: String, password: String)
+
+  private val loginForm = Form(
+    mapping(
+      "email" -> email,
+      "password" -> nonEmptyText)(Login.apply)(Login.unapply))
+
+  implicit class ResultWithToken(result: Result) {
+    def withToken(token: (String, User.Id)): Result = {
+      cacheApi.set(token._1, token._2, CacheExpiration)
+      result.withCookies(Cookie(AuthTokenCookieKey, token._1, None, httpOnly = false))
+    }
+
+    def discardingToken(token: String): Result = {
+      cacheApi.remove(token)
+      result.discardingCookies(DiscardingCookie(name = AuthTokenCookieKey))
+    }
+  }
+
+  /** Check credentials, generate token and serve it back as auth token in a Cookie */
+  def login = Action(parse.json) { implicit request =>
+    loginForm.bind(request.body).fold(
+      formErrors => BadRequest(Json.obj("err" -> formErrors.errorsAsJson)),
+      loginData => {
+        User.authenticate(loginData.email, loginData.password) map { user =>
+          val token = java.util.UUID.randomUUID().toString
+          Ok(Json.obj(
+            "id" -> user.id.toString(),
+            "email" -> user.email,
+            "name" -> user.name,
+            "active" -> user.active,
+            "approved" -> user.approved,
+            "role" -> user.role.toString)).withToken(token -> user.id)
+        } getOrElse NotFound(Json.obj("err" -> "User Not Found or Password Invalid"))
+      })
+  }
+
+  /** Invalidate the token in the Cache and discard the cookie */
+  def logout = Action { implicit request =>
+    request.headers.get(AuthTokenHeader) map { token =>
+      Redirect("/").discardingToken(token)
+    } getOrElse BadRequest(Json.obj("err" -> "No Token"))
+  }
+}
--- app/nl/amc/ebioscience/rosemary/controllers/Security.scala
+++ app/nl/amc/ebioscience/rosemary/controllers/Security.scala
@@ -1,29 +1,43 @@
 package nl.amc.ebioscience.rosemary.controllers
 
 import javax.inject._
+import play.api.mvc._
+import play.api.Logger
+import play.api.cache.CacheApi
+import play.api.libs.json.Json
+import play.mvc.Http
 import scala.concurrent.Future
-import scala.concurrent.duration._
 import play.api.libs.concurrent.Execution.Implicits._
 import nl.amc.ebioscience.rosemary.models.User
-import play.api.cache.Cache
-import play.api.data.Form
-import play.api.data.Forms.{ email, mapping, nonEmptyText }
-import play.api.libs.json.Json
-import play.api.libs.json.Json.toJsFieldJsValueWrapper
-import play.api.mvc.{ Action, BodyParser, Controller, Cookie, DiscardingCookie, Request, Result }
-import play.api.Logger
-import play.api.mvc._
-import play.mvc.Http
-import play.api.i18n.{ MessagesApi, Messages, Lang }
-import play.api.cache.CacheApi
-import play.api.Configuration
 
-trait Security { self: Controller =>
+trait Security {
+  def getUserFromToken(token: String): Either[String, User.Id]
+  def HasToken[A](p: BodyParser[A])(f: Request[A] => Result): Action[A]
+  def HasTokenAsync[A](p: BodyParser[A])(f: Request[A] => Future[Result]): Action[A]
+}
 
-  //  implicit val app: play.api.Application = play.api.Play.current
+@Singleton
+class RosemarySecurity @Inject() (cacheApi: CacheApi) extends Security with Controller {
 
   val AuthTokenHeader = "X-XSRF-TOKEN"
-  val AuthTokenCookieKey = "XSRF-TOKEN"
+
+  def getUserFromToken(token: String) = {
+    Logger.trace(Thread.currentThread().getId() + " || Token: " + token + " User: " + cacheApi.get[User.Id](token).toString)
+    cacheApi.get[User.Id](token) match {
+      case Some(user) => Right(user)
+      case None       => Left("Invalid token")
+    }
+  }
+
+  def HasToken[A](p: BodyParser[A] = parse.anyContent)(f: Request[A] => Result): Action[A] =
+    Action(p) { implicit request =>
+      auth[A, Result](request, f, (r: Result) => r)
+    }
+
+  def HasTokenAsync[A](p: BodyParser[A] = parse.anyContent)(f: Request[A] => Future[Result]): Action[A] =
+    Action.async(p) { implicit request =>
+      auth[A, Future[Result]](request, f, (r: Result) => Future(r))
+    }
 
   private def auth[A, R](request: Request[A], f: Request[A] => R, r: Result => R): R = {
     auth(request.headers) match {
@@ -34,8 +48,6 @@ trait Security { self: Controller =>
       }
     }
   }
-
-  def getUserFromToken(token: String): Either[String, User.Id]
 
   private def auth(headers: Headers): Either[String, User.Id] = {
     headers.get(AuthTokenHeader) match {
@@ -56,16 +68,6 @@ trait Security { self: Controller =>
     }
   }
 
-  def HasToken[A](p: BodyParser[A] = parse.anyContent)(f: Request[A] => Result): Action[A] =
-    Action(p) { implicit request =>
-      auth[A, Result](request, f, (r: Result) => r)
-    }
-
-  def HasTokenAsync[A](p: BodyParser[A] = parse.anyContent)(f: Request[A] => Future[Result]): Action[A] =
-    Action.async(p) { implicit request =>
-      auth[A, Future[Result]](request, f, (r: Result) => Future(r))
-    }
-
   private def readBasicAuthentication(headers: Headers): Option[Either[String, (String, String)]] = {
     headers.get(Http.HeaderNames.AUTHORIZATION).map { header =>
       val BasicHeader = "Basic (.*)".r
@@ -84,67 +86,5 @@ trait Security { self: Controller =>
         case _ => Left("Invalid Authorization header")
       }
     }
-  }
-}
-
-@Singleton
-class Authenticate @Inject() (configuration: Configuration, messagesApi: MessagesApi, cacheApi: CacheApi)
-    extends Security with Controller {
-
-  implicit def messages(implicit lang: Lang) = new Messages(lang, messagesApi)
-  
-  def getUserFromToken(token: String) = {
-    Logger.trace(Thread.currentThread().getId() + " || Token: " + token + " User: " + cacheApi.get[User.Id](token).toString)
-    cacheApi.get[User.Id](token) match {
-      case Some(user) => Right(user)
-      case None       => Left("Invalid token")
-    }
-  }
-
-  lazy val CacheExpiration =
-    configuration.getInt("cache.expiration").getOrElse(60 /*seconds*/ * 2 /* minutes */ ).seconds
-
-  case class Login(email: String, password: String)
-
-  val loginForm = Form(
-    mapping(
-      "email" -> email,
-      "password" -> nonEmptyText)(Login.apply)(Login.unapply))
-
-  implicit class ResultWithToken(result: Result) {
-    def withToken(token: (String, User.Id)): Result = {
-      cacheApi.set(token._1, token._2, CacheExpiration)
-      result.withCookies(Cookie(AuthTokenCookieKey, token._1, None, httpOnly = false))
-    }
-
-    def discardingToken(token: String): Result = {
-      cacheApi.remove(token)
-      result.discardingCookies(DiscardingCookie(name = AuthTokenCookieKey))
-    }
-  }
-
-  /** Check credentials, generate token and serve it back as auth token in a Cookie */
-  def login = Action(parse.json) { implicit request =>
-    loginForm.bind(request.body).fold(
-      formErrors => BadRequest(Json.obj("err" -> formErrors.errorsAsJson)),
-      loginData => {
-        User.authenticate(loginData.email, loginData.password) map { user =>
-          val token = java.util.UUID.randomUUID().toString
-          Ok(Json.obj(
-            "id" -> user.id.toString(),
-            "email" -> user.email,
-            "name" -> user.name,
-            "active" -> user.active,
-            "approved" -> user.approved,
-            "role" -> user.role.toString)).withToken(token -> user.id)
-        } getOrElse NotFound(Json.obj("err" -> "User Not Found or Password Invalid"))
-      })
-  }
-
-  /** Invalidate the token in the Cache and discard the cookie */
-  def logout = Action { implicit request =>
-    request.headers.get(AuthTokenHeader) map { token =>
-      Redirect("/").discardingToken(token)
-    } getOrElse BadRequest(Json.obj("err" -> "No Token"))
   }
 }
