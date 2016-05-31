77861817884fa2cf88d2cb380ecc88f8fdd3d26e
Shayan Shahand <shayanlinux@users.noreply.github.com>
2016-05-31 22:35:04 +0200

    Add ConnectionParentActor that creates ConnectionActors

---
+++ app/nl/amc/ebioscience/rosemary/actors/ConnectionParentActor.scala
@@ -0,0 +1,21 @@
+package nl.amc.ebioscience.rosemary.actors
+
+import javax.inject._
+import akka.actor.{ Actor, ActorRef, ActorLogging }
+import play.api.libs.concurrent.InjectedActorSupport
+import akka.event.LoggingReceive
+
+class ConnectionParentActor @Inject() (childFactory: ConnectionActor.Factory)
+    extends Actor with InjectedActorSupport with ActorLogging {
+  import ConnectionParentActor._
+
+  override def receive: Receive = LoggingReceive {
+    case Create(id, out) =>
+      val child: ActorRef = injectedChild(childFactory(out), s"connectionActor-$id")
+      sender() ! child
+  }
+}
+
+object ConnectionParentActor {
+  case class Create(id: String, out: ActorRef)
+}
